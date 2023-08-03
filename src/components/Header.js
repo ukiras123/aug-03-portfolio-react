bc85f5eb8e8b44909c071338f4b777fea40286d7
ukiras123 <ukiras@gmail.com>
2023-08-03 16:53:03 +1000

    Created components for different section

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="flex primary-header">
+      <div className="logo flex">
+        <div>
+          <h1>Kiran</h1>
+        </div>
+        <div>
+          <h1 className="line">Soft. Engineer</h1>
+        </div>
+      </div>
+
+      {/* <!-- top menu  --> */}
+
+      <div className="right">
+        <input type="checkbox" id="check" />
+        <label htmlFor="check" className="menu-icon">
+          <i className="fa-solid fa-bars"></i>
+        </label>
+
+        <ul className="flex navigation">
+          <li>
+            <a href="#header">Home</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#about-me">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
